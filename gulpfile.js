3dff79858a5c2a67f205a30cda9c076b03768ead
Leandro Viana <leandroviana@gmail.com>
2016-03-10 17:01:51 -0300

    +Ajustes gulpfile +Inclusão package +Criação de paginas

--- gulpfile.js
+++ gulpfile.js
@@ -6,8 +6,9 @@ var http = require('http');
 var express = require('express');
 var del = require('del');
 var runSequence = require('run-sequence');
+var compress = require('compression');
+
 var server = express();
-var s; 
 var serverport = 8080;
 
 gulp.task('clean', function() {
@@ -16,16 +17,25 @@ gulp.task('clean', function() {
 
 gulp.task('deploy', ['clean'], function() {
 	/* assets */
+	console.log('Task - deploy - fonts');
 	gulp.src('./app/assets/fonts/*.*')
     	.pipe(gulp.dest('dist/assets/fonts'));
+    console.log('Task - deploy - images');
     gulp.src('./app/assets/images/*.*')
     	.pipe(gulp.dest('dist/assets/images'));
-	gulp.src('./app/assets/css/*.*')
-    	.pipe(uglify())
-    	.pipe(gulp.dest('dist/assets/css'));
-    gulp.src('./app/assets/js/*.*')
-    	.pipe(uglify())
-    	.pipe(gulp.dest('dist/assets/js'));
+    gulp.src('./app/assets/images/escudos/*.*')
+    	.pipe(gulp.dest('dist/assets/images/escudos'));
+    gulp.src('./app/assets/images/jogadores/*.*')
+    	.pipe(gulp.dest('dist/assets/images/jogadores'));
+    gulp.src('./app/assets/images/temas/*.*')
+    	.pipe(gulp.dest('dist/assets/images/temas'));
+    console.log('Task - deploy - css');
+	gulp.src('./app/css/*.*')
+    	.pipe(gulp.dest('dist/css'));
+    console.log('Task - deploy - js');
+    gulp.src('./app/js/*.*')
+    	.pipe(gulp.dest('dist/js'));
+    console.log('Task - deploy - html');
   	gulp.src('./app/*.html')
     	.pipe(gulp.dest('dist'));
   	return;
@@ -33,29 +43,17 @@ gulp.task('deploy', ['clean'], function() {
 
 gulp.task('server', ['clean','deploy'], function() {
 
-	server.all('/index.html', function(req, res) {
-	  res.sendFile('modeloIntegracao.html', { root: 'dist' });
-	});
-
-	server.all('/js/atendimentoOnline.js', function(req, res) {
-	  res.sendFile('atendimentoOnline.js', { root: 'dist/js' });
-	});
-
-	s = http.createServer(server);
-	s.on('error', function(err){
-	  if (err.code === 'EADDRINUSE'){
-	    console.log(
-	      'Development server is already started at port ' +
-	      serverport
-	    );
-	  } else {
-	    throw err;
-	  }
+	server.use(function(req, res, next) {
+	  res.header("Access-Control-Allow-Origin", "*");
+	  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+	  next();
 	});
 
 	console.log('Acesse http://localhost:8080/index.html');
 
-	s.listen(serverport);
+	server.use(compress());
+	server.use(express.static('./dist'));
+	server.listen(serverport);
 
 });
 
